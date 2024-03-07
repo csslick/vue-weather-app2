c18f603da7b2a0b5f5c36a6bf79ad59293de2a6f
csslick <digitone@naver.com>
2024-03-07 17:50:34 +0900

    Navbar

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Navbar from './components/Navbar.vue';
+
+</script>
+
+<template>
+  <Navbar/>
+  <font-awesome-icon :icon="['fas', 'location-dot']" />
+</template>
+
+<style scoped lang="scss">
+
+</style>
